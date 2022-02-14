88476f21855b89a46a4902ef1a4a905cb9ce8799
houdacheikh <97486673+houdacheikh@users.noreply.github.com>
2022-02-14 23:40:45 +0100

    Update tests.py

--- bqsangapp/bqsang/tests.py
+++ bqsangapp/bqsang/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
 # Create your tests here.
+from http import client
+from urllib import response
+from django.test import TestCase,Client
+from django.urls import reverse
+from Stock.models import STOCK,Service,Sorti,StockEn
+import json
+# Create your tests here.
+
+
+class TestViews(TestCase):
+    def test_Myapp(self):
+        client=Client()
+
+        response = client.get(reverse('Utilisateur'))
+
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'Pages/Utilisateur.html')
